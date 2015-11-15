069eec4b2271b93ca6a9fdf0a341c540f222be05
Esha Wang <eshawang16@gmail.com>
2015-11-15 11:25:32 -0600

    Fixed indentation

--- julia.py
+++ julia.py
@@ -2,23 +2,23 @@ from pylab import *
 from numpy import NaN
 
 def julia(C):
-	X = arange(-1.5, 1.5, 0.002)
-	Y = arange(-1.5, 1.5, 0.002)
-	M = zeros((len(Y), len(X)))
+    X = arange(-1.5, 1.5, 0.002)
+    Y = arange(-1.5, 1.5, 0.002)
+    M = zeros((len(Y), len(X)))
 
-	for x_iter, x in enumerate(X):
-		for y_iter, y in enumerate(Y):
-			z = x + 1j * y
-			pixel = NaN
-			for n in range(1, 300):
-				z = z**2 + C
-				if abs(z) > 2:
-					pixel = n
-					break
+    for x_iter, x in enumerate(X):
+        for y_iter, y in enumerate(Y):
+            z = x + 1j * y
+            pixel = NaN
+            for n in range(1, 300):
+                z = z**2 + C
+                if abs(z) > 2:
+                    pixel = n
+                    break
 
-			M[y_iter, x_iter] = pixel
+            M[y_iter, x_iter] = pixel
 
-	imshow(M, cmap = cm.cubehelix, extent = (X.min(), X.max(), Y.min(), Y.max()))
-	show()
+    imshow(M, cmap = cm.cubehelix, extent = (X.min(), X.max(), Y.min(), Y.max()))
+    show()
 
 julia(-0.7 + 0.27015j)
--- mandelbrot.py
+++ mandelbrot.py
@@ -2,23 +2,23 @@ from pylab import *
 from numpy import NaN
 
 def mandelbrot(C):
-	z = 0
-	for n in range(1, 100):
-		z = z**2 + C
-		if abs(z) > 2:
-			return n
-	return NaN
+    z = 0
+    for n in range(1, 100):
+        z = z**2 + C
+        if abs(z) > 2:
+            return n
+    return NaN
 
 def plot():
-	X = arange(-2, 0.5, 0.002)
-	Y = arange(-1, 1, 0.002)
-	M = zeros((len(Y), len(X)))
+    X = arange(-2, 0.5, 0.002)
+    Y = arange(-1, 1, 0.002)
+    M = zeros((len(Y), len(X)))
 
-	for x_iter, x in enumerate(X):
-		for y_iter, y in enumerate(Y):
-			M[y_iter, x_iter] = mandelbrot(x + 1j * y)
+    for x_iter, x in enumerate(X):
+        for y_iter, y in enumerate(Y):
+            M[y_iter, x_iter] = mandelbrot(x + 1j * y)
 
-	imshow(M, cmap = cm.cubehelix, extent = (X.min(), X.max(), Y.min(), Y.max()))
-	show()
+    imshow(M, cmap = cm.cubehelix, extent = (X.min(), X.max(), Y.min(), Y.max()))
+    show()
 
 plot()
